d9fd82d49d249e74578badb3b5aa5b633e60288d
jwdietrich <jwdietrich@987cc6b6-693d-44f9-9d2b-1fccf60334b6>
2013-12-29 17:30:03 +0000

    Initial commit

---
+++ code/trunk/RandomShell/randshell.pas
@@ -0,0 +1,202 @@
+unit randshell;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  Grids, ComCtrls, Spin, ExtCtrls, Math, rDist;
+
+type
+
+  { TRandomShellForm }
+
+  TRandomShellForm = class(TForm)
+    ChisqButton: TButton;
+    CountSpin: TSpinEdit;
+    df1Label: TLabel;
+    df1Spin: TSpinEdit;
+    df2Label: TLabel;
+    df2Spin: TSpinEdit;
+    expButton: TButton;
+    fButton: TButton;
+    maxLabel: TLabel;
+    maxSpin: TSpinEdit;
+    meanLabel: TLabel;
+    meanSpin: TSpinEdit;
+    minLabel: TLabel;
+    minSpin: TSpinEdit;
+    ncpLabel: TLabel;
+    ncpSpin: TSpinEdit;
+    nLabel: TLabel;
+    OutputMemo: TMemo;
+    ParamPanel: TPanel;
+    rateLabel: TLabel;
+    rateSpin: TFloatSpinEdit;
+    RNormButton: TButton;
+    RUnifButton: TButton;
+    sdLabel: TLabel;
+    SDSpin: TSpinEdit;
+    StatPascalVectorButton: TButton;
+    StatusBar: TStatusBar;
+    SVectorButton: TButton;
+    tDistButton: TButton;
+    ValuesGrid: TStringGrid;
+    procedure expButtonClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure meanSpinChange(Sender: TObject);
+    procedure rateSpinChange(Sender: TObject);
+    procedure RNormButtonClick(Sender: TObject);
+    procedure RUnifButtonClick(Sender: TObject);
+    procedure StatPascalVectorButtonClick(Sender: TObject);
+    procedure SVectorButtonClick(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  RandomShellForm: TRandomShellForm;
+
+implementation
+
+{$R *.lfm}
+
+{ TRandomShellForm }
+
+procedure DrawGridCaptions(theGrid: TStringGrid);
+begin
+  theGrid.Cells[0, 0] := '#';
+  theGrid.Cells[1, 0] := 'Value';
+end;
+
+procedure TRandomShellForm.RUnifButtonClick(Sender: TObject);
+var
+  i, num: integer;
+  min, max: integer;
+begin
+  min := minSpin.Value;
+  max := maxSpin.Value;
+  num := CountSpin.Value;
+  ValuesGrid.Clear;
+  ValuesGrid.RowCount := num + 2;
+  DrawGridCaptions(ValuesGrid);
+  if min > max then
+    MessageDlg('min > max', mtWarning, [mbOK], 0)
+  else
+  begin
+    if num > 0 then
+      for i := 1 to num do
+      begin
+        ValuesGrid.Cells[0, i] := IntToStr(i);
+        ValuesGrid.Cells[1, i] := FloatToStr(min + random(max - min + 1));
+      end;
+  end;
+end;
+
+procedure TRandomShellForm.RNormButtonClick(Sender: TObject);
+var
+  i, num: integer;
+  mean, sd: real;
+begin
+  mean := meanSpin.Value;
+  sd := sdSpin.Value;
+  num := CountSpin.Value;
+  ValuesGrid.Clear;
+  ValuesGrid.RowCount := num + 2;
+  DrawGridCaptions(ValuesGrid);
+  if sd <= 0 then
+    MessageDlg('SD <= 0', mtWarning, [mbOK], 0)
+  else
+  begin
+    if num > 0 then
+      for i := 1 to num do
+      begin
+        ValuesGrid.Cells[0, i] := IntToStr(i);
+        ValuesGrid.Cells[1, i] := FloatToStr(randg(mean, sd));
+      end;
+  end;
+end;
+
+
+procedure TRandomShellForm.expButtonClick(Sender: TObject);
+var
+  i, num: integer;
+  min, max: integer;
+  rate: real;
+begin
+  num := CountSpin.Value;
+  rate := rateSpin.Value;
+  ValuesGrid.Clear;
+  ValuesGrid.RowCount := num + 2;
+  DrawGridCaptions(ValuesGrid);
+  if rate = 0 then
+    MessageDlg('rate = 0', mtWarning, [mbOK], 0)
+  else
+  if num > 0 then
+    for i := 1 to num do
+    begin
+      ValuesGrid.Cells[0, i] := IntToStr(i);
+      ValuesGrid.Cells[1, i] := FloatToStr(randomExp(rate));
+    end;
+end;
+
+procedure TRandomShellForm.FormCreate(Sender: TObject);
+begin
+  ValuesGrid.RowCount := 2;
+  DrawGridCaptions(ValuesGrid);
+end;
+
+procedure TRandomShellForm.meanSpinChange(Sender: TObject);
+begin
+  if meanSpin.Value <> 0 then rateSpin.Value := 1 / meanSpin.Value;
+end;
+
+procedure TRandomShellForm.rateSpinChange(Sender: TObject);
+begin
+  if rateSpin.Value <> 0 then meanSpin.Value := 1 / rateSpin.Value;
+end;
+
+procedure TRandomShellForm.StatPascalVectorButtonClick(Sender: TObject);
+var
+  i, num: integer;
+  resultString: string;
+begin
+  OutputMemo.Lines.Clear;
+  resultString := 'combine(';
+  num := ValuesGrid.RowCount;
+  if num > 2 then
+  begin
+    for i := 1 to num - 3 do
+    begin
+      resultString := resultString + ValuesGrid.Cells[1, i] + ', ';
+    end;
+    resultString := resultString + ValuesGrid.Cells[1, num - 2];
+  end;
+  resultString := resultString + ');';
+  OutputMemo.Lines.Add(resultString);
+end;
+
+procedure TRandomShellForm.SVectorButtonClick(Sender: TObject);
+var
+  i, num: integer;
+  resultString: string;
+begin
+  OutputMemo.Lines.Clear;
+  resultString := 'c(';
+  num := ValuesGrid.RowCount;
+  if num > 2 then
+  begin
+    for i := 1 to num - 3 do
+    begin
+      resultString := resultString + ValuesGrid.Cells[1, i] + ', ';
+    end;
+    resultString := resultString + ValuesGrid.Cells[1, num - 2];
+  end;
+  resultString := resultString + ')';
+  OutputMemo.Lines.Add(resultString);
+end;
+
+end.
